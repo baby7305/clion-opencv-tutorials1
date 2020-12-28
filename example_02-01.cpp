0cd9848b38ec5989f03e4c202e5a00e9c8821242
baby7305 <songhaibei7305@163.com>
2020-12-28 15:58:43 +0800

    简化代码
    补上using namespace cv;

--- example_02-01.cpp
+++ example_02-01.cpp
@@ -2,19 +2,21 @@
 //on the screen
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
+
 int main() {
 
-    cv::Mat srcImage = cv::imread("fruits.jpg");
+    Mat srcImage = cv::imread("fruits.jpg");
 
     if (!srcImage.data) {
         std::cout << "Image not loaded";
         return -1;
     }
 
-    cv::namedWindow("Example 2-1", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Example 2-1", srcImage);
-    cv::waitKey(0);
-    cv::destroyWindow("Example 2-1");
+    namedWindow("Example 2-1", cv::WINDOW_AUTOSIZE);
+    imshow("Example 2-1", srcImage);
+    waitKey(0);
+    destroyWindow("Example 2-1");
 
     return 0;
 }
